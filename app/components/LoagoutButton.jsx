0d8eac798da810599b395c4e83f6d4b4a4b9b175
Haseeb ul Rehman <haseeb.rehman@ideofuzion.com>
2024-07-25 16:48:25 +0500

    handle email update error, redirect on logout

--- app/components/LoagoutButton.jsx
+++ app/components/LoagoutButton.jsx
@@ -1,16 +1,19 @@
 "use client";
 import { usePrivy } from "@privy-io/react-auth";
 import { useUser } from "../contexts/UserContext";
+import { useRouter } from "next/navigation";
 
 const LoagoutButton = ({ onLogoutClick }) => {
   const { ready, authenticated, logout } = usePrivy();
   const { setUser } = useUser();
   // Disable logout when Privy is not ready or the user is not authenticated
+  const router = useRouter();
   const disableLogout = !ready || (ready && !authenticated);
 
   const handleLogout = async () => {
     // remove token from localstorage
     localStorage.clear();
+    router.push("/");
     setUser(null);
     await logout();
     onLogoutClick();
