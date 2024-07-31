17f0bf467db9f75dcb2b85e08d6cbc18bb4c3af6
Haseeb ul Rehman <haseeb.rehman@ideofuzion.com>
2024-07-31 16:42:47 +0500

    improve movile responsive of rules page

--- app/(withSidebar)/htp/layout.jsx
+++ app/(withSidebar)/htp/layout.jsx
@@ -1,10 +1,9 @@
 export default function HTPLayout({ children }) {
   return (
     <div className="text-white font-basement max-w-[956px] mx-auto">
-      <h1 className="text-5xl font-black text-center mb-[100px]">
+      <h1 className="text-4xl xl:text-5xl font-black mt-6 text-center mb-[100px]">
         How To Play
       </h1>
-
       {children}
     </div>
   );
--- app/(withSidebar)/htp/tab-switch.jsx
+++ app/(withSidebar)/htp/tab-switch.jsx
@@ -4,11 +4,11 @@ import { cn } from "@/lib/utils";
 
 export const TabSwitch = ({ activeId }) => {
   return (
-    <div className="mb-[85px] max-w-[600px] mx-auto h-[90px] flex border-[3px] border-secondary-200 rounded-[10px]">
+    <div className="mb-[85px] max-w-[600px] mx-auto h-[66px] xl:h-[90px] flex border-[3px] border-secondary-200 rounded-[10px]">
       <Link
         href="/htp/rules"
         className={cn(
-          "text-2xl flex-1 flex items-center justify-center font-bold",
+          "text-lg xl:text-2xl flex-1 flex items-center justify-center font-bold",
           activeId === "rule" && "rounded-r-[10px] bg-secondary-200 text-[#000]"
         )}
       >
@@ -17,7 +17,7 @@ export const TabSwitch = ({ activeId }) => {
       <Link
         href="/htp/how-to"
         className={cn(
-          "text-2xl flex-1 flex items-center justify-center font-bold",
+          "text-lg xl:text-2xl flex-1 flex items-center justify-center font-bold",
           activeId === "htp" && "rounded-l-[10px] bg-secondary-200 text-[#000]"
         )}
       >
